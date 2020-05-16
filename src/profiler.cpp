9bcc0b0f7a69de1597bf18903546b318e36e30bb
Ahmad Alobaid <ahmad88me@gmail.com>
2020-05-16 10:13:25 +0300

    save

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -36,6 +36,65 @@ void Profiler::profile_classes() {
     delete classes;
 }
 
+void Profiler::profile_properties() {
+    std::list<string>* classes = get_unprocessed_classes();
+    for(auto it=classes->cbegin();it!=classes->cend();it++){
+        profile_class_properties(*it);
+    }
+}
+
+void Profiler::profile_class_properties(string class_uri){
+    std::list<string>* properties;
+    properties = get_class_properties(class_uri);
+    // To continue
+}
+
+std::list<string>* Profiler::get_class_properties(string class_uri){
+    hdt::IteratorTripleString *itt;
+    hdt::TripleString * triple;
+    std::list<string>* properties = new std::list<string>;
+    std::list<string>* instances = get_instances(class_uri);
+    std::unordered_map<string,unsigned long> *prop_counts = new std::unordered_map<string,unsigned long>;
+    for(auto it_inst=instances->cbegin();it_inst!=instances->cend();it_inst++){
+        itt = m_hdt->search(it_inst->c_str(),"", "");
+        while(itt->hasNext()){
+            triple = itt->next();
+            if(prop_counts->find(triple->getPredicate()) != prop_counts->cend()){ // found
+                (*prop_counts)[triple->getPredicate()]++;
+            }
+            else{
+                (*prop_counts)[triple->getPredicate()] = 1;
+            }
+        }
+        delete itt;
+    }
+    m_logger->log("get_class_properties> properties counts: ");
+    for(auto it=prop_counts->cbegin();it!=prop_counts->cend();it++){
+        m_logger->log(it->first+" - "+to_string(it->second));
+        if(it->second >= min_objects){
+            properties->push_back(it->first);
+        }
+    }
+    m_logger->log("get_class_properties> included "+to_string(properties->size())+" out of "+to_string(prop_counts->size())+" properties");
+    return properties;
+}
+
+
+std::list<string>* Profiler::get_instances(string class_uri){
+    std::list<string> *instances = new std::list<string>;
+    hdt::IteratorTripleString *it = m_hdt->search("", rdf_type.c_str(),class_uri.c_str());
+    hdt::TripleString *triple;
+    while(it->hasNext()){
+        triple = it->next();
+        instances->push_back(triple->getSubject());
+    }
+    delete it;
+    m_logger->log("get_instances> num of instances: "+to_string(instances->size()));
+    return instances;
+}
+
+
+
 void Profiler::set_dir_sep(string sep) {
     m_dir_sep = sep;
 }
@@ -62,7 +121,7 @@ std::list<string>* Profiler::get_classes_from_hdt() {
     while(itt->hasNext()) {
         triple = itt->next();
         class_uri = triple->getSubject();
-//        cout<<class_uri<<endl;
+        //        cout<<class_uri<<endl;
         if(class_uri.substr(0, dbo_prefix.size())==dbo_prefix) {
             classes->push_back(class_uri);
         }
@@ -72,9 +131,34 @@ std::list<string>* Profiler::get_classes_from_hdt() {
     return classes;
 }
 
+std::list<string>* Profiler::get_unprocessed_classes() {
+    std::list<string>* classes = new std::list<string>;
+    ifstream input;
+    string line, token="", tt;
+    istringstream ist;
+    m_logger->log("get_classes_from_txt> before the loop");
+    input.open(merge_dirs(get_properties_dir(), get_classes_fname()));
+    while(getline(input, line)) {
+        ist.str(line);
+        while(getline(ist, tt, '/')) {
+            if(tt!="") {
+                token = tt;
+            }
+        }
+        if(!file_exists(merge_dirs(get_properties_dir(), token+".txt"))){
+            classes->push_back(line);
+        }
+        else{
+            m_logger->log("get_classes_from_txt> "+line+" is already computed");
+        }
+    }
+    m_logger->log("get_classes_from_txt> number of fetched classes: "+to_string(classes->size()));
+    return classes;
+}
+
 void Profiler::write_classes(std::list<string>* classes) {
     string classes_fdir;
-    classes_fdir = merge_dirs(m_properties_dir,m_classes_fname);
+    classes_fdir = merge_dirs(m_properties_dir, m_classes_fname);
     m_logger->log("write_classes> to write classes to: "+classes_fdir);
     ofstream out_file;
     out_file.open(classes_fdir);
@@ -84,25 +168,32 @@ void Profiler::write_classes(std::list<string>* classes) {
     out_file.close();
 }
 
-string Profiler::merge_dirs(string base_dir, string relative_dir){
+string Profiler::merge_dirs(string base_dir, string relative_dir) {
     string full_dir;
     full_dir = base_dir;
-    if(full_dir.back()!=this->m_dir_sep.back()){
+    if(full_dir.back()!=this->m_dir_sep.back()) {
         full_dir += m_dir_sep;
     }
-    if(relative_dir.front()==m_dir_sep.back()){
+    if(relative_dir.front()==m_dir_sep.back()) {
         full_dir += relative_dir.substr(1);
     }
-    else{
+    else {
         full_dir += relative_dir;
     }
     return full_dir;
 }
 
-void Profiler::set_properties_dir(string p_dir){
+void Profiler::set_properties_dir(string p_dir) {
     m_properties_dir = p_dir;
 }
 
-string Profiler::get_properties_dir(){
+string Profiler::get_properties_dir() {
     return m_properties_dir;
 }
+
+bool Profiler::file_exists(string fdir){
+    ifstream input;
+    input.open(fdir.c_str());
+    return input.good();
+}
+
--- src/profiler.hpp
+++ src/profiler.hpp
@@ -3,8 +3,10 @@
 
 #include <list>
 #include <string>
+#include <sstream>
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
 #include <HDT.hpp>
 #include <HDTManager.hpp>
 #include <easy_logger/easy_logger.h>
@@ -16,7 +18,7 @@ const string rdfs_class = "http://www.w3.org/2000/01/rdf-schema#Class";
 const string rdf_type = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
 const string rdfs_subclassof = "http://www.w3.org/2000/01/rdf-schema#subClassOf";
 const string dbo_prefix = "http://dbpedia.org/ontology/";
-
+const string dbp_predix = "http://dbpedia.org/property/";
 
 class Profiler{
   public:
@@ -27,7 +29,12 @@ class Profiler{
     void set_logger(EasyLogger* logger);
     void set_logger(string);
     void profile_classes();
+    void profile_properties();
     std::list<string>* get_classes_from_hdt();
+    std::list<string>* get_unprocessed_classes();
+    void profile_class_properties(string);
+    std::list<string>* get_instances(string class_uri);
+    std::list<string>* get_class_properties(string class_uri);
     void set_dir_sep(string sep);
     string get_dir_sep();
     string merge_dirs(string base_dir, string relative_dir);
@@ -35,6 +42,7 @@ class Profiler{
     string get_properties_dir();
     void set_classes_fname(string);
     string get_classes_fname();
+    bool file_exists(string);
   private:
     void write_classes(std::list<string>*);
     EasyLogger* m_logger=nullptr;
@@ -42,5 +50,6 @@ class Profiler{
     string m_classes_fname="classes.txt";
     string m_properties_dir="generated_files";
     string m_dir_sep="/";
+    double min_objects=2;
 };
 #endif // PROF_H
